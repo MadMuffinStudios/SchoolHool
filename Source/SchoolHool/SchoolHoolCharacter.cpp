7d58564ad2e1fef8dd129481d132f431788523d6
Ali <mdsaidov98@gmail.com>
2025-05-11 04:28:56 +0500

    camera fix (WIP)

--- Source/SchoolHool/SchoolHoolCharacter.cpp
+++ Source/SchoolHool/SchoolHoolCharacter.cpp
@@ -10,6 +10,7 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
+#include "Components/BoxComponent.h"
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
@@ -20,7 +21,7 @@ ASchoolHoolCharacter::ASchoolHoolCharacter()
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
+	
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
